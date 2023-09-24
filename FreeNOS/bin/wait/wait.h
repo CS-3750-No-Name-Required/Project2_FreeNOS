e774e3e5d927121d9d7f1efe8db0c2feafb7b944
ikenton <isabellakenton@gmail.com>
2023-09-24 02:25:47 -0700

    created main.cpp & wait.h

---
+++ FreeNOS/bin/wait/wait.h
@@ -0,0 +1,12 @@
+#ifndef __BIN_WAIT_WAIT_H
+#define __BIN_WAIT_WAIT_H
+
+#include <POSIXApplication.h>
+Class Wait : public POSIXApplication
+{
+  public:
+  	Wait(int argc, char **argv);
+  	
+ }
+ #endif
+
